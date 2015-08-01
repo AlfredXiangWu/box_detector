39f4503566352b1a2d4f9cf5b83e177c1115010d
AlfredXiangWu <alfredxiangwu@gmail.com>
2015-08-01 11:30:23 +0800

    foxconn data process, gt crop function is finished

--- src/util/word_alignment.m
+++ src/util/word_alignment.m
@@ -1,9 +1,39 @@
-function res = word_alignment(img, step)
+function [res] = word_alignment(img, step)
     [height, width] = size(img);
     
-    binary = img > 0.8;
+    binary = img > 0.7;
     
-    idx = [];
+%     idx = [];
+%     v_avg = mean(binary, 1);   
+%     idx = find(v_avg < 0.98);
+%     if isempty(idx)
+%         res = binary;
+%         position.hstart = 0;
+%         position.hend = height;
+%         position.wstart = 0;
+%         position.wend = width;
+%         return;
+%     end
+%     wstart = max(idx(1) - 4, 1);
+%     wend = min(idx(end) + 4, width);
+% 
+%     
+%     idx = [];
+%     h_avg = mean(binary, 2);
+%     idx = find(h_avg < 0.98);
+%     if isempty(idx)
+%         res = binary;
+%         position.hstart = 0;
+%         position.hend = height;
+%         position.wstart = 0;
+%         position.wend = width;
+%         return;
+%     end
+%     hstart = max(idx(1) - 4, 1);
+%     hend = min(idx(end) + 4, height);
+
+    
+%     idx = [];
     tmp = ones(length((1+step):(height-step)), 1);
     for i = (1+step):(width-step)
         tmp = tmp & binary((1+step):(height-step), i);
@@ -31,6 +61,10 @@ function res = word_alignment(img, step)
     
     res = [];
     res = img(hstart:hend, wstart:wend);
-    
+%     position.hstart = hstart;
+%     position.hend = hend;
+%     position.wstart = wstart;
+%     position.wend = wend;
+%     
 end
 
